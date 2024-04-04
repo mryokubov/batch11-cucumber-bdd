0d8d9f16cf6ca5e1f41963844150f4cff0d2c453
TairovichJR <yokubov.omonjon@gmail.com>
2024-04-03 21:28:17 -0400

    implemented step defs with automation code closes #5

--- src/test/java/com/academy/techcenture/step_definitions/NopCommerceStepDefinitions.java
+++ src/test/java/com/academy/techcenture/step_definitions/NopCommerceStepDefinitions.java
@@ -1,30 +1,56 @@
 package com.academy.techcenture.step_definitions;
 
+import com.academy.techcenture.config.ConfigReader;
+import com.academy.techcenture.driver.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class NopCommerceStepDefinitions {
 
+    private WebDriver driver;
+
     @Given("user is on the homepage")
     public void user_is_on_the_homepage() {
-        System.out.println("user is on the home page");
+        driver = Driver.getDriver();
+        driver.get(ConfigReader.getProperty("url"));
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals("Home page titles do not match", actualTitle, "nopCommerce demo store");
     }
     @When("user clicks on the login link")
     public void user_clicks_on_the_login_link() {
-        System.out.println("user clicks on the login link");
+        WebElement loginLink = driver.findElement(By.linkText("Log in"));
+        Assert.assertTrue("Login link is not displayed", loginLink.isDisplayed());
+        loginLink.click();
     }
     @Then("user should be navigate to login page")
     public void user_should_be_navigate_to_login_page() {
-        System.out.println("user is navigated to login page");
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals("Login page titles do not match", actualTitle, "nopCommerce demo store. Login");
+        WebElement loginHeader = driver.findElement(By.className("page-title"));
+        Assert.assertTrue("Login Header is not displayed",loginHeader.isDisplayed());
     }
     @When("user uses login credentials {string} and {string} and clicks on login button")
     public void user_uses_login_credentials_and_and_clicks_on_login_button(String username, String password) {
-        System.out.println("using " + username + " and " + password + " for login");
+        WebElement emailInput = driver.findElement(By.id("Email"));
+        emailInput.sendKeys(username);
+        WebElement passwordInput = driver.findElement(By.id("Password"));
+        passwordInput.sendKeys(password);
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@class,'login-button')]"));
+        Assert.assertTrue(loginButton.isEnabled());
+        loginButton.click();
     }
     @Then("user should be able to log in")
     public void user_should_be_able_to_log_in() {
-        System.out.println("user is logged in");
+        WebElement myAccountLink = driver.findElement(By.linkText("My account"));
+        Assert.assertTrue(myAccountLink.isDisplayed());
+        WebElement logOutLink = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logOutLink.isDisplayed());
     }
 
 }
